74f2aa936843393fd47340a52030f189634e56ec
Karl Frazier <darthfrazier350@gmail.com>
2014-11-15 13:58:28 -0500

    This is a test to see if I can commit from Xcode directly

---
+++ NextBeat/SongTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SongTableViewCell.swift
+//  NextBeat
+//
+//  Created by Karl  Frazier on 11/15/14.
+//  Copyright (c) 2014 Karl  Frazier. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
